b603814ae46d03367c5245b403a2e695599a39b8
derekntnguyen <79728577+derekntnguyen@users.noreply.github.com>
2023-11-05 01:37:50 -0400

    Adds code and experiment

---
+++ millions-of-regressions.py
@@ -0,0 +1,47 @@
+import time
+
+import numpy as np
+import statsmodels.api as sm
+
+# Generate random data
+num_samples = 10
+num_features = 3
+X = np.random.rand(num_samples, num_features)
+y = np.random.rand(num_samples)
+
+# Perform linear regressions using matrix stacking
+start_time = time.time()
+
+num_regressions = 1000
+X_stacked = np.tile(X, (num_regressions, 1, 1))
+print(X_stacked.shape)
+y_stacked = np.tile(y, (num_regressions, 1))
+print(y_stacked.shape)
+
+print(X_stacked.transpose(0, 2, 1).shape)
+
+results = (
+    np.linalg.inv(X_stacked.transpose(0, 2, 1) @ X_stacked)
+    @ X_stacked.transpose(0, 2, 1)
+    @ y_stacked[..., np.newaxis]
+)
+matrix_time = time.time() - start_time
+print("Matrix time taken: %s seconds" % (matrix_time))
+
+print("Results using NumPy and linear algebra:")
+print(results[0])
+
+# Loop the same task using statsmodels and time it
+start_time = time.time()
+for i in range(num_regressions):
+    model = sm.OLS(y, X)
+    results = model.fit()
+
+ols_time = time.time() - start_time
+print("\nOLS time taken: %s seconds" % (ols_time))
+
+print("Results using statsmodels:")
+print(results.params)
+
+# Calculate the speed improvement between the two methods
+print("\nSpeed improvement: %sx" % (ols_time / matrix_time))
